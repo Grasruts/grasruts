30c296e316b230a2eafc83d4853212878d4ec836
sushant12 <sussyoung9@gmail.com>
2018-10-19 19:06:57 +0545

    revert old mailer setting for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,14 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.default_url_options = { host: 'grasruts.com' }
   # config.action_mailer.smtp_settings = {
   #   address:              ENV['SMTP_ADDRESS'],
@@ -39,7 +46,7 @@ Rails.application.configure do
   #   password:             ENV['SMTP_PASSWORD'],
   #   enable_starttls_auto: true
   # }
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_deliveries = true
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_options = { from: 'contact@grasruts.com' }
 
