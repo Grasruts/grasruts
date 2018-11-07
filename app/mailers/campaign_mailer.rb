f1c7960490e4779047bc8363c91e0786835d33d2
sushant12 <sussyoung9@gmail.com>
2018-11-07 10:35:08 +0545

    update send to email for new campaign notification

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -4,6 +4,6 @@ class CampaignMailer < ApplicationMailer
   def review_email(id)
     @campaign = Campaign.find_by_uuid id
     @campaign_url = "https://www.grasruts.com/#{@campaign.uri}"
-    mail(to: 'contact@gmail.com', subject: 'Grasruts: Campaign Approval Request')
+    mail(to: 'contact@grasruts.com', subject: 'Grasruts: Campaign Approval Request')
   end
 end
